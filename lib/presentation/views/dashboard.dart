62870816172be083c9b50b8be1e9656e055d5ba8
Dell <saicharan.teamup@gmail.com>
2025-07-07 13:48:32 +0530

    committed by charan 07/07/2025 at 1:45 PM

--- lib/presentation/views/dashboard.dart
+++ lib/presentation/views/dashboard.dart
@@ -52,7 +52,7 @@ class _DashboardState extends State<Dashboard> {
             });
           },
           physics: const NeverScrollableScrollPhysics(),
-          children: [HomeScreen(),ProfileScreen(),],
+          children: [HomeScreen(), ProfileScreen()],
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: Container(
