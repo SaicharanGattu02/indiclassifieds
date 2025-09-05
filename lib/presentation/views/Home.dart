bcda2158b617815b6a61afc05100a542a454ec5a
ozrit <varun@ozrit.com>
2025-09-05 18:18:59 +0530

    committed by varun at office 05-09-2025 6:12 PM

--- lib/presentation/views/Home.dart
+++ lib/presentation/views/Home.dart
@@ -107,6 +107,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ),
                           ],
+
                         );
                       },
                     ),
