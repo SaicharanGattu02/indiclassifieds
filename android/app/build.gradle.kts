972a194fca1a5a2e8e0692fbab4d252a273863d9
ozrit <varun@ozrit.com>
2025-09-11 18:14:53 +0530

    committed by varun at office 11-09-2025 06:14 PM

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.ind.classifieds"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // ✅ explicit NDK version
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
